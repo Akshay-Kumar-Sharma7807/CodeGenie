a54e9b37327ed9b5fc0ccd4eea30e1d61213c563
Akshay Kumar Sharma <sharmaakshaykumar7807@gmail.com>
2023-02-26 13:58:30 +0000

    Commit made from VS Code Azure Static Web Apps

--- client/codegenie/src/Layout.tsx
+++ client/codegenie/src/Layout.tsx
@@ -4,7 +4,7 @@ import Header from './components/Header'
 
 export default function Layout() {
     return (
-        <div>
+        <div className=''>
             <Header></Header>
         </div>
     )
